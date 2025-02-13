99963f33577a1cfa2c17984f0842fa1599913e1f
Ed Ball <ed.ball@logos.com>
2025-02-13 08:26:38 -0800

    WIP

--- tools/Benchmarks/DataMapperBenchmark.cs
+++ tools/Benchmarks/DataMapperBenchmark.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using Faithlife.Data;
 using Faithlife.Data.BulkInsert;
@@ -12,18 +13,24 @@ public class DataMapperBenchmark : IDisposable
 	{
 		m_connector = DbConnector.Create(new SqliteConnection("Data Source=:memory:"), new DbConnectorSettings { AutoOpen = true, LazyOpen = true });
 		m_connector.Command("drop table if exists DataMapperBenchmark;").Execute();
-		m_connector.Command("create table DataMapperBenchmark (ItemId integer primary key, IntegerValue integer not null);").Execute();
+		m_connector.Command("create table DataMapperBenchmark (ItemId integer primary key, Number integer not null);").Execute();
 
+		const int recordCount = 50000;
 		m_connector
-			.Command("insert into DataMapperBenchmark (IntegerValue) values (@IntegerValue)...;")
-			.BulkInsert(Enumerable.Range(0, 50000).Select(x => DbParameters.Create("IntegerValue", x)));
+			.Command("insert into DataMapperBenchmark (AnInteger, AReal, AString, ABlob) values (@AnInteger, @AReal, @AString, @ABlob)...;")
+			.BulkInsert(Enumerable.Range(1, recordCount)
+				.Select(x => DbParameters.Create(
+					("AnInteger", x < recordCount ? x : null),
+					("AReal", x < recordCount ? 1.0 / x : null),
+					("AString", x < recordCount ? $"string{x}" : null),
+					("ABlob", x < recordCount ? Encoding.UTF8.GetBytes($"blob{x}") : null))));
 	}
 
 	[Benchmark]
-	public long Int64() => m_connector.Command("select IntegerValue from DataMapperBenchmark;").Enumerate<long>().Last();
+	public long Int64() => m_connector.Command("select AnInteger from DataMapperBenchmark;").Enumerate<long>().Last();
 
 	[Benchmark]
-	public long? NullableInt64() => m_connector.Command("select IntegerValue from DataMapperBenchmark;").Enumerate<long?>().Last();
+	public long? NullableInt64() => m_connector.Command("select AnInteger from DataMapperBenchmark;").Enumerate<long?>().Last();
 
 	public void Dispose() => m_connector.Dispose();
 
