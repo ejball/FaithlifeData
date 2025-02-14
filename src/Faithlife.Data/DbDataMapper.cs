a1b25254c97515f093ec57eca1438d086c626baf
Ed Ball <ed.ball@logos.com>
2025-02-13 17:35:10 -0800

    WIP

--- src/Faithlife.Data/DbDataMapper.cs
+++ src/Faithlife.Data/DbDataMapper.cs
@@ -85,7 +85,16 @@ public abstract class DbDataMapper
 				return (DbTypeMapper<T>) (object) new ObjectMapper();
 
 			if (TupleInfo.IsTupleType(typeof(T)))
-				return new TupleMapper<T>(this);
+			{
+				var tupleTypes = typeof(T).GetGenericArguments();
+				var tupleMapperType = tupleTypes.Length switch
+				{
+					2 => typeof(TupleMapper<,>),
+					////3 => typeof(TupleMapper<,,>),
+					_ => null,
+				};
+				return (DbTypeMapper<T>) (tupleMapperType is null ? new TupleMapper<T>(this) : Activator.CreateInstance(tupleMapperType.MakeGenericType(tupleTypes), [.. tupleTypes.Select(GetTypeMapper)])!);
+			}
 
 			if (typeof(T).IsEnum)
 				return new EnumMapper<T>();
@@ -309,6 +318,91 @@ public abstract class DbDataMapper
 		private readonly IReadOnlyList<IDbTypeMapper>? m_tupleTypeMappers;
 	}
 
+	private sealed class TupleMapper<T1, T2>(DbTypeMapper<T1> mapper1, DbTypeMapper<T2> mapper2) : TypeMapper<(T1, T2)>
+	{
+		public override int? FieldCount => mapper1.FieldCount + mapper2.FieldCount;
+
+		protected override (T1, T2) MapCore(IDataRecord record, int index, int count)
+		{
+			if (FieldCount is { } requiredFieldCount && count != requiredFieldCount)
+				throw BadFieldCount(count);
+
+			var valueCount = 2;
+#if !NETSTANDARD2_0
+			Span<(int Index, int Count)> valueRanges = stackalloc (int Index, int Count)[valueCount];
+#else
+			var valueRanges = new (int Index, int Count)[valueCount];
+#endif
+			var recordIndex = index;
+			for (var valueIndex = 0; valueIndex < valueCount; valueIndex++)
+			{
+				var mapperFieldCount = valueIndex == 0 ? mapper1.FieldCount : mapper2.FieldCount;
+
+				int fieldCount;
+				int? nullIndex = null;
+				if (mapperFieldCount is null)
+				{
+					int? remainingFieldCount = 0;
+					var minimumRemainingFieldCount = 0;
+					for (var nextValueIndex = valueIndex + 1; nextValueIndex < valueCount; nextValueIndex++)
+					{
+						var nextFieldCount = nextValueIndex == 0 ? mapper1.FieldCount : mapper2.FieldCount;
+						if (nextFieldCount is not null)
+						{
+							remainingFieldCount += nextFieldCount.Value;
+							minimumRemainingFieldCount += nextFieldCount.Value;
+						}
+						else
+						{
+							remainingFieldCount = null;
+							minimumRemainingFieldCount += 1;
+						}
+					}
+
+					if (remainingFieldCount is not null)
+					{
+						fieldCount = count - recordIndex - remainingFieldCount.Value;
+					}
+					else
+					{
+						for (var nextRecordIndex = recordIndex + 1; nextRecordIndex < count; nextRecordIndex++)
+						{
+							if (record.GetName(nextRecordIndex).Equals("NULL", StringComparison.OrdinalIgnoreCase))
+							{
+								nullIndex = nextRecordIndex;
+								break;
+							}
+						}
+
+						if (nullIndex is not null)
+						{
+							fieldCount = nullIndex.Value - recordIndex;
+						}
+						else if (count - (recordIndex + 1) == minimumRemainingFieldCount)
+						{
+							fieldCount = 1;
+						}
+						else
+						{
+							throw new InvalidOperationException($"Tuple item {valueIndex} must be terminated by a field named 'NULL': {Type.FullName}");
+						}
+					}
+				}
+				else
+				{
+					fieldCount = mapperFieldCount.Value;
+				}
+
+				valueRanges[valueIndex] = (recordIndex, fieldCount);
+				recordIndex = nullIndex + 1 ?? recordIndex + fieldCount;
+			}
+
+			return (
+				mapper1.Map(record, valueRanges[0].Index, valueRanges[0].Count),
+				mapper2.Map(record, valueRanges[1].Index, valueRanges[1].Count));
+		}
+	}
+
 	private abstract class SingleFieldMapper<T> : TypeMapper<T>
 	{
 		public override int? FieldCount => 1;
@@ -422,6 +516,9 @@ public abstract class DbDataMapper
 	{
 		protected override byte[] MapNotNullField(IDataRecord record, int index)
 		{
+			if (record.GetValue(index) is byte[] blob)
+				return blob;
+
 			var byteCount = (int) record.GetBytes(index, fieldOffset: 0, buffer: null, bufferoffset: 0, length: 0);
 			var bytes = new byte[byteCount];
 			record.GetBytes(index, fieldOffset: 0, buffer: bytes, bufferoffset: 0, length: byteCount);
@@ -436,10 +533,10 @@ public abstract class DbDataMapper
 			if (record is DbDataReader dbReader)
 				return dbReader.GetStream(index);
 
-			var byteCount = (int) record.GetBytes(index, 0, null, 0, 0);
+			var byteCount = (int) record.GetBytes(index, fieldOffset: 0, buffer: null, bufferoffset: 0, length: 0);
 			var bytes = new byte[byteCount];
-			record.GetBytes(index, 0, bytes, 0, byteCount);
-			return new MemoryStream(bytes, 0, byteCount, writable: false);
+			record.GetBytes(index, fieldOffset: 0, buffer: bytes, bufferoffset: 0, length: byteCount);
+			return new MemoryStream(buffer: bytes, index: 0, count: byteCount, writable: false);
 		}
 	}
 
